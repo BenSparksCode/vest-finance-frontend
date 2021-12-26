bb4b5737e11dce936928b0c208bd8b2584653f0a
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2021-12-26 17:12:28 +0200

    More CSS in JSX for Layout

--- components/Layout.js
+++ components/Layout.js
@@ -3,19 +3,6 @@ import Head from "next/head";
 import Header from "./Header";
 import NavBar from "./NavBar";
 
-const layoutStyle = {
-  display: "flex",
-  flexDirection: "column",
-  height: "100%",
-  width: "100%",
-};
-
-const contentStyle = {
-  flex: 1,
-  display: "flex",
-  flexDirection: "column",
-};
-
 const Layout = (props) => (
   <div className="Layout">
     <Head>
@@ -23,10 +10,32 @@ const Layout = (props) => (
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta charSet="utf-8" />
     </Head>
+
     <Header />
     <div className="Content">{props.children}</div>
     <NavBar />
-    <style jsx>{`
+    <style jsx global>{`
+      * {
+        box-sizing: border-box;
+      }
+
+      html,
+      body,
+      #__next {
+        height: 100%;
+        width: 100%;
+      }
+
+      body {
+        margin: 0;
+        padding: 0;
+        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto",
+          "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans",
+          "Helvetica Neue", sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+      }
+
       .Layout {
         display: flex;
         flex-direction: column;
@@ -38,6 +47,7 @@ const Layout = (props) => (
         flex: 1;
         display: flex;
         flex-direction: column;
+        font-family: Arial;
       }
     `}</style>
   </div>
