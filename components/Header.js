5e8d837d394527c4cdbae3c01bd2ec4c11386c7e
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2021-12-27 07:10:40 +0200

    Got basic connect wallet button working with useDapp

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,30 @@
 import Link from "next/link";
-
-const handleConnectWalletClicked = (e) => {
-  console.log(e, "ahashkdfs");
-};
+import {
+  Mainnet,
+  DAppProvider,
+  useEtherBalance,
+  useEthers,
+  Config,
+} from "@usedapp/core";
+import { formatEther } from "@ethersproject/units";
 
 const Header = (props) => {
+  const { activateBrowserWallet, account } = useEthers();
+  const etherBalance = useEtherBalance(account);
+
+  const handleConnectWalletClicked = (e) => {
+    console.log("Connecting to wallet...");
+    activateBrowserWallet();
+  };
+
   return (
     <div>
       <Link href="/">
         <div className="Header">{props.appTitle}</div>
       </Link>
       <button onClick={handleConnectWalletClicked}>Connect Wallet</button>
+      {account && <p>Account: {account}</p>}
+      {etherBalance && <p>Balance: {formatEther(etherBalance)}</p>}
     </div>
   );
 };
--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,28 @@
 import Link from "next/link";
+import { Mainnet, DAppProvider } from "@usedapp/core";
 
 import Layout from "../components/Layout";
 import LandingView from "./landing";
 
 import "../styles.scss";
 
+const config = {
+  readOnlyChainId: Mainnet.chainId,
+  readOnlyUrls: {
+    [Mainnet.chainId]:
+      "https://mainnet.infura.io/v3/3165a249c65f4198bf57200109b8fadf",
+  },
+};
+
 const App = () => (
-  <Layout>
-    <br />
-    <Link href="/">
-      <a>Welcome to Vest Finance</a>
-    </Link>
-  </Layout>
+  <DAppProvider config={config}>
+    <Layout>
+      <br />
+      <Link href="/">
+        <a>Welcome to Vest Finance</a>
+      </Link>
+    </Layout>
+  </DAppProvider>
 );
 
 export default App;
